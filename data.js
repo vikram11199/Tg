95ed6ac08133f2a49fbd7fe5e15f12b3388b7685
vikram11199 <namekumar0707@gmail.com>
2026-03-06 16:18:24 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+export default function handler(req, res) {
+
+const file = fs.readFileSync("Telegram_27.csv","utf8");
+
+const lines = file.split("\n");
+
+let result = [];
+
+lines.forEach(line => {
+
+const parts = line.split("|");
+
+if(parts.length > 5){
+
+result.push({
+id: parts[0],
+name: parts[1],
+user_id: parts[3],
+telegram_id: parts[4],
+username: parts[5]
+});
+
+}
+
+});
+
+res.status(200).json(result);
+
+  }
